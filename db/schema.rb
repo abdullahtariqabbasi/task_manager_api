7507fbf8bfef5dcec161edebe51d8b7ecfbf52cc
Abdullah Tariq Abbasi <abdullahabbasi852@gmail.com>
2024-03-11 20:41:08 +0500

    Upgraded to rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_07_183355) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_03_11_153656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
