853a1ac57304b69755a44a98eb53b976ce03f5fa
Abdullah Tariq Abbasi <abdullahabbasi852@gmail.com>
2024-03-11 22:57:20 +0500

    Added user management APIs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  def authenticate_user!
+    token = request.headers['Authorization']&.split(' ')&.last
+    if token.present?
+      payload = JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256').first
+      @current_user = User.find_by(id: payload['user_id'])
+    end
+    render json: { error: 'Unauthorized' }, status: :unauthorized unless @current_user.present?
+  end
 end
